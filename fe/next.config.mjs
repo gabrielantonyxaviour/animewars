b2dec15e7bd9c172955a6c33db7d9f0e4e26a2bb
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2024-07-13 01:10:59 +0200

    init frontned and add boxes

---
+++ fe/next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "app.dynamic.xyz",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "noun-api.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
